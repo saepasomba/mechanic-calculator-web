ce7d0dd49146ae3343abcdacf0138ee0438982a0
Sae Pasomba <saepasomba12@gmail.com>
2025-02-21 11:52:05 +0700

    feat(adsense): add adsense metatag and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import MainLayout from "@/layouts/MainLayout";
 import { Providers } from "./providers";
 import { Analytics } from "@vercel/analytics/react";
+import React from "react";
 
 export default function RootLayout({
   children,
@@ -9,9 +10,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <Providers>
-          <MainLayout>{children}</MainLayout>
+    <head>
+      <meta name="google-adsense-account" content="ca-pub-3484021851390412"/>
+      <title>Mekalkulator</title>
+    </head>
+    <body>
+    <Providers>
+      <MainLayout>{children}</MainLayout>
         </Providers>
         <Analytics />
       </body>
