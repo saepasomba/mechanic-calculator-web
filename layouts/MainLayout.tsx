d67929ada312c86accb72884b6775b75b7910ae8
Sae Pasomba <saepasomba12@gmail.com>
2025-02-21 21:50:02 +0700

    feat(layout): add highlight text on top of the page

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
-import { Center, Box, useColorMode, IconButton, Flex } from "@chakra-ui/react";
-import React, { ReactNode, useEffect, useState } from "react";
+import { Center, Box, useColorMode, IconButton, Flex, Text, Link } from "@chakra-ui/react";
+import React, { cloneElement, ReactElement, ReactNode, useEffect, useState } from "react";
 
 interface MainLayoutInterface {
   children: ReactNode;
@@ -28,6 +28,24 @@ export default function MainLayout({ children }: MainLayoutInterface) {
       maxW="100%"
       position="relative"
     >
+      <HighlightText text={
+        <Text>
+          Dukung saya dengan memberikan like di {" "}
+          <Link
+            href={"https://jogjagamers.org/topic/320807-guidetool-website-mechanic-calculator-⚙%EF%B8%8F👨%E2%80%8D🔧/"}
+            target="_blank">
+            <Text
+              as={"span"}
+              fontWeight={"bold"}
+              textDecor={"underline"}
+              cursor={""}
+              display="inline-block"
+            >
+              forum Jogjagamers ini
+            </Text>
+          </Link> 👍
+        </Text>
+      }/>
       <Box
         minH="100vh"
         maxW="25rem"
@@ -48,3 +66,20 @@ export default function MainLayout({ children }: MainLayoutInterface) {
     </Center>
   );
 }
+
+const HighlightText = ({ text }: { text: ReactElement<typeof Text> }) => {
+
+  return (
+    <Box
+      bgGradient="linear(to-r, gray.200, gray.300, gray.200)"
+      _dark={{ bgGradient: "linear(to-r, gray.700, gray.600, gray.700)" }}
+      py={1}
+      w="100%"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      {cloneElement(text as ReactElement, { textAlign: "center", fontSize: "small", opacity: 0.8 })}
+    </Box>
+  );
+};
